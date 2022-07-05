a875c02a153fa785db17066f974a2aea6bbdf7c2
John Doe <lees13579@tukorea.ac.kr>
2022-07-05 14:43:54 +0900

    multiple func

--- main.c
+++ main.c
@@ -1,3 +1,12 @@
 void main(){
-    
+
+}
+
+void multiplication_table(){
+    for (int i=2;i<10;i++){
+        for(int j=2;j<10;j++)
+        {
+            printf("%d x %d = %d",i,j,i*j);
+        }
+    }
 }
